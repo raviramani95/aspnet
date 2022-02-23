489fcc545faf1912b35e871c01ef57a4b36e2001
raviramani95 <ravi.ramani@oneadvanced.com>
2022-02-23 10:18:08 +0530

    9 seed user data table add photos table not work

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Extensions;
 
 namespace API.Entities
 {
@@ -14,5 +16,20 @@ namespace API.Entities
 
         public byte[] PasswordSalt { get; set; }
         
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastActivity { get; set; } = DateTime.Now;
+        public String Gender { get; set; }
+        public String Introduction { get; set; }
+        public String LookingFor { get; set; }
+        public String Interests { get; set; }
+        public String City { get; set; }
+        public String Country { get; set; }
+        public ICollection<Photos> MyProperty { get; set; }
+
+        public int GetAge(){
+            return DateOfBirth.CalculateAge();
+        }
     }
 }
