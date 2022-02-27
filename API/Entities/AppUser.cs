8c4905b561c947173a48650f933c5df5cf98d022
raviramani95 <ravi.ramani@oneadvanced.com>
2022-02-27 10:45:31 +0530

    15.0 up to 187 done

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -30,5 +30,8 @@ namespace API.Entities
 
         public ICollection<UserLike> LikeByUsers { get; set; }
         public ICollection<UserLike> LikedUsers { get; set; }
+
+        public ICollection<Message> MessageSent { get; set; }
+        public ICollection<Message> MessageReceived { get; set; }
     }
 }
